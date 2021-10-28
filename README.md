18d9f5a53581abe54203785537874a0adf592c65
AnttonLA <anttonlamarca@gmail.com>
2021-10-28 18:24:52 +0200

    add csv parsing for tempTrender tempOnDay

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # MNXB01-project
-Repo for the final project of the MNXB01 course at Lund University. Autumn 2021
+Repo for the final project of the MNXB01 course at Lund University. Autumn 2021.
+
+Group H
+	Jesper Larsson
+	Antton Lamarca
+
+##Usage
+
+As of now, the way I've been running the functions is the following
+
+    root
+    tempTrender* test_2 = new tempTrender("../datasets/smhi-opendata_1_162870_20210926_101011_Lulea.csv")
+    test_2->tempOnDay(1,2)
+
+Make sure to include the local path to your include/ directory in the rootlogon.C file for it to work!
+
--- code/src/tempTrender.cpp
+++ code/src/tempTrender.cpp
@@ -1,12 +1,57 @@
 #include <iostream>
+#include<fstream>
+#include <sstream>
+#include<TH1F.h> 
 #include "tempTrender.h"
 
-tempTrender::tempTrender(const std::string& filePath) {
-	std::cout << "The user supplied " << filePath <<" as the path to the data file.\n";
-	std::cout << "You should probably store this information in a member variable of the class! Good luck with the project! :)\n";
+using namespace std;
+
+tempTrender::tempTrender(const string& filePath) : _path{filePath}{ //filePath has now been stored in member variable _path
+  cout << "The user supplied " << filePath <<" as the path to the data file.\n";
+  //std::cout << "You should probably store this information in a member variable of the class! Good luck with the project! :)\n";
+
 }
 
-// void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const {} //Make a histogram of the temperature on this day
+void tempTrender::tempOnDay(int monthToCalculate, int dayToCalculate) const { //Make a histogram of the temperature on this day
+
+  cout<<"I am stil working!" << endl;
+  //Code to open the csv files
+  fstream fin; //File pointer
+  fin.open(_path, ios::in); //Open file at '_path'
+  if (fin.fail()) { //Check that file can be opened.
+    cout << "File could not be opened. Please check that the provided path is correct." << endl;
+	return;
+  }
+
+  vector<string> row;
+  string line, cell;
+  string delimiter=";";
+  
+  int i = 0;
+  int j = 0;
+  while (getline(fin, line)){ //read whole file, row by row, store line in variable 'line' each loop
+    i++;
+    row.clear();
+
+    stringstream lineStream(line);
+	while (lineStream.good() && getline(lineStream, cell, ';')) {
+		j++;
+        row.push_back(cell);
+    }
+
+	if(i%10000==0){
+	  cout << "line " << i << ", at date " << row[0] << " the temperature was " << row[2] << endl;
+    }
+    j=0;
+  }
+
+
+  // CREATE HISTOGRAM that we will fill with data from specified date
+	//(name, title;xlabel;ylabel, bins, xmin, xmax)
+  //TH1D* hDayTemp = new TH1D("one_day_tempt", ";Temperature [C]; Entries", 100, -20, 40); 
+
+  //hDayTemp->Draw();
+}
 // void tempTrender::tempOnDay(int dateToCalculate) const {} //Make a histogram of the temperature on this date
 // void tempTrender::tempPerDay() const {} //Make a histogram of the average temperature of each day of the year
 // void tempTrender::hotCold() const {} //Make a histogram of the hottest and coldest day of the year
