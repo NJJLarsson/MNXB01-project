344a7a1443fc45651e50ec75ca96972dd251fa29
Ennio Mara <enniomara@users.noreply.github.com>
2021-11-02 21:19:50 +0100

    Implement measurementFromLine

--- code/src/measurement.cpp
+++ code/src/measurement.cpp
@@ -1,6 +1,21 @@
 #include "../include/measurement.h"
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 Measurement::Measurement(std::string date, double measurement)
     : date(date), measurement(measurement) {}
+
+Measurement measurementFromLine(std::string line) {
+    std::stringstream ss(line);
+    std::vector<std::string> row;
+    std::string cell;
+
+    while(ss.good() && std::getline(ss, cell, ';')){
+        // Initialize and add <colname, int vector> pairs to result
+        row.push_back(cell);
+    }
+
+    return Measurement(row[0], std::stod(row[2]));
+}
